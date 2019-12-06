552fc296d9e9ad57f163d849b340115b3080cf07
Bruce Thomas <response.write@gmail.com>
2019-12-06 21:43:26 +0000

    generic world creator

--- frontend/src/boxes.js
+++ frontend/src/boxes.js
@@ -1,14 +1,27 @@
-import { MeshBuilder, Vector3 } from 'babylonjs';
+import {MeshBuilder, Vector3} from 'babylonjs';
 import materials from './materials';
 
 export default boxes;
 function boxes({scene}) {
-    const { CreateBox } = MeshBuilder;
-    const { wire } = materials(scene);
+    const {CreateBox} = MeshBuilder;
+    const {wire} = materials(scene);
 
     var name;
-    var dim = { size: 0.25 };
-    var cubes = {};
+    var dim = {size: 0.25};
+    var cubes = {
+        /*
+        id: index
+        x: -1, 0, 1
+        y: 0,1,2
+        z: 0 to 8
+        mesh: Mesh
+
+        */
+    };
+
+
+    console.log(createWorld());
+
     var items = [1, 1, 1, 2, 2, 2, 3, 3, 3];
     items.forEach((v, i) => {
         name = `box_${v}_${i}_${i % 3}`;
@@ -33,3 +46,27 @@ function boxes({scene}) {
     console.log(cubes);
     return cubes;
 }
+
+
+
+    function createWorld(size = 0, origin = true) {
+
+        const sizes = [3, 5, 7, 9]; // small, medium, large, extra-large
+        const columns = sizes[size] || sizes[0];
+        const squared = columns ** 2; // number of cells per face
+        const cubed = columns ** 3; // total number of cells
+        const offset = origin ? (columns / 2) >> 0 : 0; // offset to origin
+
+        let world = [...Array(cubed)].map((_, i) => {
+            // setup coordinates
+
+            const x = (i % columns) - offset;
+            const y = (((i / columns) >> 0) % columns) - offset;
+            const z = (((i / squared) >> 0) % columns) - offset;
+
+            // console.log('V:%s  X:%s   Y:%s   Z:%s', i, x, y, z);
+            return {id: i, x, y, z};
+        });
+
+        return world;
+    }
