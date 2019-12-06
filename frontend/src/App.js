6cd70f04773695dce33895b49258b1ff09c1ef50
Bruce Thomas <response.write@gmail.com>
2019-12-06 19:05:07 +0000

    added root height/width CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
+
+// import logo from './logo.svg';
 // import scene from './scene';
-//
+
 
 import Scene from './Scene';
 
