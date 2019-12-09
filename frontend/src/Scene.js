3a9c220cda72a461a0d2d8195ef2e61312368b0d
Bruce Thomas <response.write@gmail.com>
2019-12-09 13:01:57 +0000

    fixed place rotation

--- frontend/src/Scene.js
+++ frontend/src/Scene.js
@@ -11,7 +11,6 @@ import { getTerrain } from './terrain';
 
 class Scene extends React.Component {
     sceneDidMount = generateScene.bind(this);
-
     render() {
         return <Canvas sceneDidMount={this.sceneDidMount} />;
     }
@@ -27,59 +26,19 @@ function generateScene(e) {
 
     engine.runRenderLoop(() => scene && scene.render());
 
-    // This creates and positions a free camera (non-mesh)
     createCamera(e, (diameter + 1) * 3.6);
     createLight(e, 'hemi', 'sun', 0.5, [0.5, 0.5, 0.5], [0.5, 0.5, 0.5]);
     // createLight(e, 'point', 'point1', 0.8, [0, 6, -1]);
 
     showAxis((diameter + 1) * 3, scene);
+
     const earth = getMatrix(e, diameter);
-
-    var CoR_At = new Vector3(0, 0, 0);
-    var sphere = MeshBuilder.CreateSphere('sphere', { diameter: 0.25 }, scene);
-    sphere.position = CoR_At;
-
     earth.filter(c => !c.core).forEach((cube, i) => getTerrain(cube, e, 0));
 
-    // // TicTakToe tech-ton-nix  tec-ton-ics
-
-    // A.forEach(key => {
-    //     var s = 3.5;
-    //     var box = boxes[key];
-    //     // box.parent = sphere;
-    //     box.scaling = new Vector3(s, s, s);
-    //     box.material = m['matA'];
-    // });
-
-    // sphere.animations = [];
-    // sphere.animations.push(anim)
-    //
 
     window.rotate = function(axis, rotations = 1, z = 0) {
         return rotatePlane(scene, earth, axis, rotations, z);
     }
     window.earth = earth;
-    window.turn = function(dim = 'x', n = 1) {
-        let array = Array(9);
-        array = [...array].map((_, i) => i + 9 + 9);
-        array.forEach(n => {
-            var box = earth['C' + (n % 9)];
-            box.parent = sphere;
-        });
-        var anim = new Animation(
-            'spin',
-            `rotation.${dim}`,
-            25,
-            Animation.ANIMATIONTYPE_FLOAT,
-            Animation.ANIMATIONLOOPMODE_CONSTANT
-        );
 
-        var frames = [
-            { frame: 0, value: 0 },
-            { frame: 100, value: Math.PI / n }
-        ];
-
-        anim.setKeys(frames);
-        scene.beginDirectAnimation(sphere, [anim], 0, 25 * 4, false);
-    };
 }
--- frontend/src/rotatePlane.js
+++ frontend/src/rotatePlane.js
@@ -2,33 +2,35 @@ import { Animation, Vector3, MeshBuilder, StandardMaterial } from 'babylonjs';
 
 export default rotatePlane;
 function rotatePlane(scene, matrix, axis = 'y', rotations = 2, z = 0) {
-
-    if(!rotations) return;
+    if (!rotations) return;
 
     const speed = 0.46;
     const direction = rotations > 0 ? 1 : -1;
     const amount = [0.5, 1, 1.5, 2][(Math.abs(rotations) - 1) % 4];
     const duration = 100 * amount * speed;
-    const origin = new Vector3(0,0,0);
-    
-    let sphere = MeshBuilder.CreateSphere('sphere', { diameter: 8, segments: 2 }, scene);
-    
-    const mat = new StandardMaterial("origin", scene);
+    const origin = new Vector3(0, 0, 0);
+
+    let sphere = MeshBuilder.CreateSphere(
+        'sphere',
+        { diameter: 8, segments: 2 },
+        scene
+    );
+
+    const mat = new StandardMaterial('origin', scene);
     mat.wireframe = true;
     sphere.material = mat;
     sphere.position = origin;
 
-    const collection = matrix.filter(cube => {
+    let collection = matrix.filter(cube => {
         const child = cube[axis] === z;
-        if(child) cube.mesh.parent = sphere;
+        cube.mesh.setParent(null);
+        if (child) cube.mesh.setParent(sphere);
         return child;
     });
 
+    console.log(collection.length);
 
-
-    console.log(collection);
-
-    var anim = new Animation(
+    const anim = new Animation(
         'spin',
         `rotation.${axis}`,
         25,
@@ -36,9 +38,49 @@ function rotatePlane(scene, matrix, axis = 'y', rotations = 2, z = 0) {
         Animation.ANIMATIONLOOPMODE_CONSTANT
     );
 
+    // console.log(collection[0].mesh.absolutePosition);
     // var frames = [{ frame: 0, value: 0 }, { frame: 100, value: Math.PI / rotations }];
-    var frames = [{ frame: 0, value: 0 }, { frame: duration, value: Math.PI * amount * direction }];
+    const frames = [
+        { frame: 0, value: 0 },
+        { frame: duration, value: Math.PI * amount * direction }
+    ];
+    const onAnimationEnd = () => {
+        collection = updateCubePositions(collection, matrix, sphere);
+        sphere.dispose(true);
+    };
 
     anim.setKeys(frames);
-    scene.beginDirectAnimation(sphere, [anim], 0, duration, false);
+    scene.beginDirectAnimation(
+        sphere,
+        [anim],
+        0,
+        duration,
+        false,
+        1,
+        onAnimationEnd
+    );
+}
+
+function updateCubePositions(array, matrix, parent) {
+    // after the animation, update the positions to absolute coords
+    // and update the respective matrix references too
+    return array
+        .map(cube => {
+            const { absolutePosition } = cube.mesh;
+            let { x, y, z } = absolutePosition;
+            const { mesh } = cube;
+
+            // the matrix attributes
+            x = Math.round(absolutePosition.x);
+            y = Math.round(absolutePosition.y);
+            z = Math.round(absolutePosition.z);
+
+            mesh.setParent(null);
+            mesh.setAbsolutePosition(new Vector3(x,y,z));
+
+            return { ...cube, mesh, x, y, z };
+        })
+        .forEach(cube => {
+            matrix[cube.id] = cube;
+        });
 }
