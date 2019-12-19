87970b6f31dac8b30c82e036fe60e8876fe147de
Bruce Thomas <response.write@gmail.com>
2019-12-19 13:37:16 +0000

    updated exploder

--- frontend/src/explodeMatrix.js
+++ frontend/src/explodeMatrix.js
@@ -1,49 +1,53 @@
-import { Vector3, Space } from 'babylonjs';
+import {Vector3, Space} from 'babylonjs';
+import BezierEasing from 'bezier-easing';
 
 export default explodeMatrix;
-function explodeMatrix({ scene, earth }, score, amount = 2, duration = 250) {
+function explodeMatrix({scene, earth}, score, amount = 2, duration = 250) {
     let players = {}; // playerId indexed dictionary
-    score.rank.forEach((p, i) => (players[p.playerId] = { ...p, rank: i }));
+    score.rank.forEach((p, i) => (players[p.playerId] = {...p, rank: i}));
     const winner = score.rank[0].playerId;
 
     earth
         .filter(cube => cube.owner || cube.owner === winner)
         .map(cube => {
-            cube.mesh.displace = players[cube.owner].rank ** 1.5;
+            cube.mesh.displace = players[cube.owner].rank;
             return cube.mesh;
         })
         .forEach(m => {
-            const { x, y, z } = m.absolutePosition;
-            const { displace } = m;
+            const {x, y, z} = m.absolutePosition;
+            const {displace} = m;
 
             if (!displace) return;
 
-            animationStack(scene, m, [x, y, z], displace * amount, duration);
+            animationStack(scene, m, [x, y, z], displace, duration);
         });
 }
 
 function animationStack(scene, mesh, xyz, size, time) {
+    var easing = BezierEasing(0.13, 0.69, 0.58, 1);
     const [x, y, z] = xyz;
-    for (let i = 0; i <= time; i += (time / 10) >> 0) {
-        const n = Number(i);
-        setTimeout(() => {
-            const temp = (n / time) * size;
-            mesh.translate(new Vector3(x, y, z), temp, Space.WORLD);
-            if (n >= time) {
-                rotationLoop(scene, () => {
-                    const array = [rnd(3, -3), rnd(1, -1), 0];
-                    const spin = rnd(5, 0.1) * 1024;
-                    const [x, y, z] = array;
-                    mesh.rotate(
-                        new Vector3(x, y, z),
-                        Math.PI / spin,
-                        Space.LOCAL
-                    );
-                });
-            }
-        }, Number(i));
+    const frames = 30;
+    const steps = (time / frames) >> 0;
+    const total = steps * frames;
+    const pos = new Vector3(x, y, z);
+    let done = false;
+
+    for (let i = 0; i <= total; i += steps) {
+        const temp = (easing(i / total) * 1000 >> 0) / 1000;
+        // console.log(i, size, temp * size);
+        setTimeout(() => mesh.translate(pos, temp * size, Space.WORLD), i);
+
+        if (i >= total && !done) {
+            rotationLoop(scene, () => {
+                done = true;
+                const spin = rnd(5, 0.1) * 1024;
+                const [x, y, z] = [rnd(3, -3), rnd(1, -1), 0];
+                mesh.rotate(new Vector3(x, y, z), Math.PI / spin, Space.LOCAL);
+            });
+        }
     }
 }
+
 function rotationLoop(scene, fn) {
     scene.registerBeforeRender(fn);
 }
